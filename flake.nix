854192a11b0c61aba2a49dbab77cee77f6da8ad8
Lucas David Traverso <lucas6246@gmail.com>
2026-02-17 00:26:00 -0300

    Add flake with deps

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+          pnpm
+        ];
+      };
+    }
+  );
+}
